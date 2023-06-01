28aeb0bd8b0ea311aa9cb049bb001cf7bf8ebdb1
Gabriel Marques Neves <107442931+0GabrielMarques0@users.noreply.github.com>
2023-06-01 23:20:57 +0000

    fucional

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-//Escreva aqui o JS
+fetch('https://api.github.com/users/0gabrielmarques0/repos')
+.then( res =>  res.json())
+.then( res => {
+    res.forEach(repo => {
+        const li = document.createElement('li')
+        const name = document.createTextNode(repo.name)
+        li.appendChild(name)
+        const lista = document.querySelector(".lista")
+        lista.appendChild(li)
+    });
+    
+})
