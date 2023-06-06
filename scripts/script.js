86e301dddeda497febbf13086be602e26732d903
Gabriel Marques Neves <107442931+0GabrielMarques0@users.noreply.github.com>
2023-06-06 00:30:46 +0000

    trabalhando na impressão de users

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,46 @@
-function teste() {
-    console.log("funciona")
-}
+const users = []
 
-fetch('https://api.github.com/users/0gabrielmarques0')
-.then( res =>  res.json())
-.then( res => {
+const buttonUser = document.querySelector(".user-button")
+
+buttonUser.addEventListener("click" , function () {
+    const userGit = document.querySelector(".user").value
+    fetch(`https://api.github.com/users/${userGit}`)
+    .then( res =>  res.json())
+    .then( res => {
+        const box = document.querySelector("article")
+        box.style.display = "block"
+
+        const user = {
+            nome: res.login,
+            imagem: res.avatar_url,
+            link: res.html_url
+       }
+       users.push(user)
+    })
+})
+
+
+
+users.map( function (user){
+    
         const img = document.createElement('img')
-        img.setAttribute("src", res.avatar_url)
+        img.setAttribute("src", user.imagem)
         img.style.width = "100px"
         img.style.height = "100px"
         const userPic = document.querySelector(".user-pic")
-        userPic.appendChild(img)    
+        userPic.appendChild(img)
 
+        const texto = document.createElement('h1')
+        texto.textContent = user.nome
+        texto.style.color = "white"
+        const userName = document.querySelector(".user-name")
+        userName.appendChild(texto)
+
+        const link = document.createElement('a')
+        link.setAttribute("href", user.link)
+        link.setAttribute("target", "_blank")
+        link.textContent = "Github"
+        link.style.color = "white"
+        const userLink = document.querySelector('.user-link')
+        userLink.appendChild(link)
 })
